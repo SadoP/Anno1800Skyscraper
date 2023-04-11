a87d3c06d994123419fc5649199762b47770ade3
SadoP <37416439+SadoP@users.noreply.github.com>
2023-04-11 21:49:02 +0200

    add saving figures

--- utils/figures.py
+++ utils/figures.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 
 
@@ -12,3 +14,30 @@ def open_figure(my_dpi=96, size=800, **kwargs) -> (plt.Figure, plt.Axes):
     fig = plt.figure(figsize=(size / my_dpi, size / my_dpi), dpi=my_dpi, **kwargs)
     axis = fig.add_subplot(111)
     return fig, axis
+
+
+def save_figure(fig: plt.figure, filename, size: tuple = (30, 30), dpi=600, **kwargs):
+    """
+    Saves as figure to file. Creates directory if necessary. Figure size given in cm.
+    :param fig: Figure object
+    :param filename: Filename
+    :param size: length and width in cm
+    :param dpi: DPI of the resulting graphic
+    :param kwargs: formats for file extensions as list, defaults to saving as png, pdf and svg
+    :return: None
+    """
+    filename = str(filename)
+    cm = 1 / 2.54
+    x = size[0] * cm
+    y = size[1] * cm
+    fig.set_size_inches(x, y, forward=True)
+    fig_path = os.path.split(filename)[0]
+    if not os.path.exists(fig_path):
+        os.makedirs(fig_path)
+    if kwargs.get("formats"):
+        for f in kwargs.pop("formats"):
+            fig.savefig(f"{filename}.{f}", dpi=dpi, **kwargs)
+        return
+    fig.savefig(filename + ".svg", dpi=dpi, **kwargs)
+    fig.savefig(filename + ".png", dpi=dpi, **kwargs)
+    fig.savefig(filename + ".pdf", dpi=dpi, **kwargs)
